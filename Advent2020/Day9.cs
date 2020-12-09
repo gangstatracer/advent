cd4a3b18655f59b48c33e87b566eb0244b9482e7
iperevoschikov <iperevoschikov@skbkontur.ru>
2020-12-09 11:38:22 +0500

    day9.2

--- Advent2020/Day9.cs
+++ Advent2020/Day9.cs
@@ -8,10 +8,13 @@ namespace Advent2020
     public class Day9 : TestBase
     {
         [Test]
-        public void Task1()
+        public void Task()
         {
             var numbers = ReadLines().Select(long.Parse).ToArray();
-            Console.WriteLine(FindOdd(25, numbers));
+            var odd = FindOdd(25, numbers);
+            Console.WriteLine(odd);
+            var weakness = FindWeakness(odd, numbers);
+            Console.WriteLine(weakness);
         }
 
         [Test]
@@ -22,6 +25,31 @@ namespace Advent2020
                 35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576
             };
             Assert.AreEqual(127, FindOdd(5, numbers));
+            Assert.AreEqual(62, FindWeakness(127, numbers));
+        }
+
+        private static long FindWeakness(long odd, IReadOnlyList<long> numbers)
+        {
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                var sum = numbers[i];
+                var min = numbers[i];
+                var max = numbers[i];
+                for (var j = i + 1; j < numbers.Count; j++)
+                {
+                    min = Math.Min(min, numbers[j]);
+                    max = Math.Max(max, numbers[j]);
+                    sum = checked(sum + numbers[j]);
+
+                    if (sum > odd)
+                        continue;
+
+                    if (sum == odd)
+                        return min + max;
+                }
+            }
+
+            throw new Exception();
         }
 
         private static long FindOdd(int preamble, IReadOnlyList<long> numbers)
