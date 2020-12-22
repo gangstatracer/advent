a414de4b505c4cc506b18298a1a88d3cb9838188
iperevoschikov <iperevoschikov@skbkontur.ru>
2020-12-22 12:10:20 +0500

    day22.2

--- Advent2020/Day22.cs
+++ Advent2020/Day22.cs
@@ -14,14 +14,93 @@ namespace Advent2020
                 .Split(Environment.NewLine + Environment.NewLine)
                 .Select(p => new LinkedList<int>(p.Split(Environment.NewLine).Skip(1).Select(int.Parse)))
                 .ToArray();
-            
-            var winner1 = CrabCombat(decks);
-            Console.WriteLine(Score(winner1));
+
+            Console.WriteLine(Score(CrabCombat(decks)));
         }
+
+        [Test]
+        public void Task2()
+        {
+            var decks = ReadAll()
+                .Split(Environment.NewLine + Environment.NewLine)
+                .Select(p => new LinkedList<int>(p.Split(Environment.NewLine).Skip(1).Select(int.Parse)))
+                .ToArray();
+
+            var winner = RecursiveCombat(decks);
+            Console.WriteLine(Score(decks[winner]));
+        }
+
+        [Test]
+        public void Task2Example()
+        {
+            var decks = new[]
+            {
+                new LinkedList<int>(new[] {9, 2, 6, 3, 1}),
+                new LinkedList<int>(new[] {5, 8, 4, 7, 10}),
+            };
+            var winner = RecursiveCombat(decks);
+            Assert.AreEqual(1, winner);
+            Assert.AreEqual(291, Score(decks[winner]));
+        }
+
 
         private static long Score(IReadOnlyCollection<int> deck)
         {
-            return deck.Select((c, i) => (long)c * (deck.Count - i)).Sum();
+            return deck.Select((c, i) => (long) c * (deck.Count - i)).Sum();
+        }
+
+        private static int RecursiveCombat(IReadOnlyList<LinkedList<int>> decks)
+        {
+            var roundsHashes = new HashSet<string>();
+            while (decks.Count(d => d.Any()) > 1 && !roundsHashes.Contains(GetHash(decks)))
+            {
+                roundsHashes.Add(GetHash(decks));
+                var turn = new int[decks.Count];
+                var maxValue = 0;
+                var maxIndex = 0;
+                var startRecursion = true;
+                for (var i = 0; i < decks.Count; i++)
+                {
+                    if (decks[i].Any())
+                    {
+                        var card = decks[i].First!.Value;
+                        turn[i] = card;
+                        decks[i].RemoveFirst();
+                        if (card > maxValue)
+                        {
+                            maxIndex = i;
+                            maxValue = card;
+                        }
+
+                        if (decks[i].Count < card)
+                            startRecursion = false;
+                    }
+                }
+
+                if (startRecursion)
+                    maxIndex = RecursiveCombat(decks
+                        .Select((t, i) => new LinkedList<int>(t.Take(turn[i])))
+                        .ToArray());
+
+
+                decks[maxIndex].AddLast(turn[maxIndex]);
+                for (var i = 0; i < turn.Length; i++)
+                {
+                    if (i != maxIndex && turn[i] != 0)
+                        decks[maxIndex].AddLast(turn[i]);
+                }
+            }
+
+            for (var i = 0; i < decks.Count; i++)
+                if (decks[i].Any())
+                    return i;
+
+            throw new Exception();
+        }
+
+        private static string GetHash(IEnumerable<LinkedList<int>> decks)
+        {
+            return string.Join(";", decks.Select(d => string.Join(",", d)));
         }
 
         private static LinkedList<int> CrabCombat(IReadOnlyList<LinkedList<int>> decks)
